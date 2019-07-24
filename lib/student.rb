a7e10e8c072a1c229d63a73ec82370134b9104ca
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-24 23:55:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,9 +65,11 @@ class Student
       FROM students 
       WHERE students.grade = 9
     SQL
- 
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
+    
+    grade_9 = []
+    DB[:conn].execute(sql).select |row|
+      new = self.new_from_db(row)
+      grade_9 << new
     end.first
   end 
 end
