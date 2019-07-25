c91f56156466acbcfe84ba1121a7e0dd60ce6c0c
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-25 00:02:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,7 +91,7 @@ class Student
     below_12
   end 
   
-  def self.first_x_students_in_grade_10(num) 
+  def self.first_X_students_in_grade_10(num) 
      sql = <<-SQL
       SELECT * 
       FROM students 
